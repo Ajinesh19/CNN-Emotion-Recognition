56b78fccd7fe0f584f92a60273a178bd36f8c916
anson10 <sansonmsa@gmail.com>
2024-07-30 13:32:48 +0530

    new code added

--- ajinesh.py
+++ ajinesh.py
@@ -13,3 +13,33 @@ while True:
       break
 car.release()
 cv2.destroyAllWindows ()
+
+class ResidualUnit(tf.keras.layers.Layer):
+  def __init__(self, filters, strides=1, activation="relu", **kwargs):
+    super().__init__(**kwargs)
+    self.activation = tf.keras.activations.get(activation)
+    self.main_layers = [
+        DefaultConv2d(filters, strides=strides),
+        tf.keras.layers.BatchNormalization(),
+        self.activation,
+        DefaultConv2d(filters),
+        tf.keras.layers.BatchNormalization()
+    ]
+
+    self.skip_layers = []
+    if strides > 1:
+      self.skip_layers = [
+          DefaultConv2d(filters, kernel_size=1, strides=strides),
+          tf.keras.layers.BatchNormalization()
+      ]
+
+      def call(self, inputs):
+        Z = inputs
+        for layer in self.main_layers:
+          Z = layer(Z)
+
+        skip_Z = inputs
+        for layer in self.skip_layers:
+          skip_Z = layer(skip_Z)
+
+        return self.activation(Z + skip_Z)
